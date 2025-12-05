fa7e541f65ccfee765947aefc3b37b2f2b5d32f7
RaivoR <schnee.grsp@gmail.com>
2025-12-05 11:43:58 +0200

    Pievienoju Tailwind, sagatavoju to izmantošanai

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Zivju Veikals</title>
-     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-     @stack('styles')
+     <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+     @stack('styles') -->
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
